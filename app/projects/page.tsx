7829e368f3ef75f5de990852b8a530898f013927
HildeH2002 <r0839760@student.thomasmore.be>
2024-03-14 10:53:49 +0100

    build

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,11 @@
 import { type Metadata } from 'next'
 import Image from 'next/image'
-
+/* 
 import logoAnimaginary from '@/images/logos/animaginary.svg'
 import logoCosmos from '@/images/logos/cosmos.svg'
 import logoHelioStream from '@/images/logos/helio-stream.svg'
 import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoPlanetaria from '@/images/logos/planetaria.svg' */
 import { SimpleLayout } from '../components/SimpleLayout'
 import { Card } from '../components/Card'
 
@@ -15,35 +15,35 @@ const projects = [
     description:
       'Creating technology to empower civilians to explore space on their own terms.',
     link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
+    /* logo: logoPlanetaria, */
   },
   {
     name: 'Animaginary',
     description:
       'High performance web animation library, hand-written in optimized WASM.',
     link: { href: '#', label: 'github.com' },
-    logo: logoAnimaginary,
+    /* logo: logoAnimaginary, */
   },
   {
     name: 'HelioStream',
     description:
       'Real-time video streaming library, optimized for interstellar transmission.',
     link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
+    /* logo: logoHelioStream, */
   },
   {
     name: 'cosmOS',
     description:
       'The operating system that powers our Planetaria space shuttles.',
     link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
+    /* logo: logoCosmos, */
   },
   {
     name: 'OpenShuttle',
     description:
       'The schematics for the first rocket I designed that successfully made it to orbit.',
     link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
+    /* logo: logoOpenShuttle, */
   },
 ]
 
@@ -76,11 +76,11 @@ export default function Projects() {
         {projects.map((project) => (
           <Card as="li" key={project.name}>
             <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image
-                src={project.logo}
+              <img
+                src="https://www.freepik.com/free-photos-vectors/funny-face"
                 alt=""
                 className="h-8 w-8"
-                unoptimized
+                /* unoptimized */
               />
             </div>
             <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
